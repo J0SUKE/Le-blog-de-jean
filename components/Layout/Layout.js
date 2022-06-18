f01ba17821bcbcaf458f54059fef6236f54855ea
Jean <chakib3500@gmail.com>
2022-06-18 17:06:01 +0200

    head

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Header from '../Header/Header';
 import styles from './Layout.module.scss';
 import { useRouter } from 'next/router'
 import Footer from '../Footer/Footer';
+import Head from 'next/head';
 
 
 export default function Layout({children}) {
@@ -21,6 +22,9 @@ export default function Layout({children}) {
   
     return (
         <>
+            <Head>
+                <link rel="icon" type="image/x-icon" href="favicon.svg"/>
+            </Head>
             <Header scrollbarWidth={scrollbarWidth}/>
             <main className={styles.parallax_wrapper} ref={parallax}>
                 {children}
