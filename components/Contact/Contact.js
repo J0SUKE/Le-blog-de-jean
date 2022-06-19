c19a5a7a598b7133930fda7231ca04ad55a4f190
Jean <chakib3500@gmail.com>
2022-06-19 12:21:51 +0200

    social medias

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,17 +1,17 @@
 import styles from './Contact.module.scss';
 
-export default function Contact() {
+export default function Contact({social_medias}) {
   return (
     <div className={styles.content_conatainer}>
         <div className={styles.content}>
             <h2>Pour me contacter</h2>
             <p>Vous pouvez me joindre via les reseaux sociaux ou me contacter directement par mail sur 
-                <a href="mailto:jeanmazouni@gmail.com"> jeanmazouni@gmail.com</a>
+                <a href={`mailto:${social_medias.email}`}> {social_medias.email}</a>
             </p>
             <ul>
-                <a href='https://twitter.com/Jean_M_____I' target='_blank' rel='noreferrer'><li><img src="/images/twitter.svg" alt="" /></li></a>
-                <a href='https://github.com/J0SUKE' target='_blank' rel='noreferrer'><li><img src="/images/github.svg" alt="" /></li></a>
-                <a href="mailto:jeanmazouni@gmail.com"><li><img src="/images/email.svg" alt="" /></li></a>
+                <a href={social_medias.twitter} target='_blank' rel='noreferrer'><li><img src="/images/twitter.svg" alt="" /></li></a>
+                <a href={social_medias.github} target='_blank' rel='noreferrer'><li><img src="/images/github.svg" alt="" /></li></a>
+                <a href={`mailto:${social_medias.email}`}><li><img src="/images/email.svg" alt="" /></li></a>
             </ul>
         </div>
     </div>
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,40 @@
 import Contact from "../components/Contact/Contact"
 import BlogLayout from '../components/BlogLayout/BlogLayout';
 
-export default function ContactPage() {
+export default function ContactPage({social_medias}) {
+
   return (
     <BlogLayout>
-        <Contact/>
+        <Contact social_medias={social_medias}/>
     </BlogLayout>
   )
+}
+
+export async function getStaticProps(params) {
+  let social_medias = await fetch(`${process.env.NEXT_PUBLIC_SOCIAL_MEDIAS}/api/social-medias`);
+  social_medias = await social_medias.json();
+
+  social_medias = social_medias.data;
+
+  social_medias = social_medias.map(({attributes})=>{
+    return (
+      {
+        name : attributes.name,
+        link : attributes.link
+      }
+    )
+    })
+  let social_mediasObj = {}
+  social_medias.forEach(element => {
+    social_mediasObj[element.name] = element.link
+  });
+
+  social_medias = social_mediasObj;
+
+
+  return {
+    props:{
+      social_medias
+    }
+  }
 }
