7b2e5124fc825442c6aa84cbe3cc87d7ff587726
Jean <chakib3500@gmail.com>
2022-07-18 14:43:30 +0200

    404

---
+++ components/404/Page404.js
@@ -0,0 +1,27 @@
+import styles from './Page404.module.scss';
+import BlogLayout from '../BlogLayout/BlogLayout';
+import Link from 'next/dist/client/link';
+
+export default function Page404() {
+  return (
+    <BlogLayout>  
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.content__left}>
+              <h1>Page introuvable</h1>
+              <p>La page que vous cherchez n'existe pas ou a été supprimée</p>
+              <Link href={'/blog'}>
+                <a>
+                  <button>Explorer</button>
+                </a>
+              </Link>
+              
+          </div>
+          <div className={styles.content__right}>
+            <img src="/images/404.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </BlogLayout>    
+  )
+}
---
+++ pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Page404 from '../components/404/Page404'
+
+export default function Page404Container() {
+  return (
+    <Page404/>
+  )
+}
