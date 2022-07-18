d20abd15fe63af45a563f72c7934c489f872511e
Jean <chakib3500@gmail.com>
2022-07-18 14:48:02 +0200

    &apos;

--- components/404/Page404.js
+++ components/404/Page404.js
@@ -9,7 +9,7 @@ export default function Page404() {
         <div className={styles.content}>
           <div className={styles.content__left}>
               <h1>Page introuvable</h1>
-              <p>La page que vous cherchez n'existe pas ou a été supprimée</p>
+              <p>La page que vous cherchez n&apos;existe pas ou a été supprimée</p>
               <Link href={'/blog'}>
                 <a>
                   <button>Explorer</button>
